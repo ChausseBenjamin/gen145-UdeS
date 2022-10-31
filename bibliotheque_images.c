56466e223b83bc8ae4f3d1a25231709e1e1fe160
Benjamin Chausse <benjamin@chausse.xyz>
2022-10-31 09:55:16 -0400

    Initial commit

---
+++ bibliotheque_images.c
@@ -0,0 +1,23 @@
+/********
+Fichier: bibliotheque_images.c
+Auteurs: Benjamin Chausse - chab1704
+         <++>
+Date: 31 Octobre 2022
+Description: Fichier de distribution pour GEN145.
+********/
+
+#include "bibliotheque_images.h"
+
+int pgm_lire(char nom_fichier[], int matrice[MAX_HAUTEUR][MAX_LARGEUR],
+             int *p_lignes, int *p_colonnes,
+             int *p_maxval, struct MetaData *p_metadonnees)
+{
+    return OK;
+}
+
+int pgm_ecrire(char nom_fichier[], int matrice[MAX_HAUTEUR][MAX_LARGEUR],
+               int lignes, int colonnes,
+               int maxval, struct MetaData metadonnees)
+{
+    return OK;
+}
