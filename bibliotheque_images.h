d2f91c22e28862f37ef618f2b7a41d8b5054184d
Benjamin Chausse <benjamin@chausse.xyz>
2022-10-31 11:12:58 -0400

    Text alignment

--- bibliotheque_images.h
+++ bibliotheque_images.h
@@ -36,7 +36,7 @@ Description: Solution de la problematique pour GEN145.
 
 struct MetaData{
     char auteur[MAX_CHAINE];
-    char dateCreation[MAX_CHAINE]; // yyyy-mm-dd 
+    char dateCreation[MAX_CHAINE]; // yyyy-mm-dd
     char lieuCreation[MAX_CHAINE];
 };
 
@@ -48,22 +48,98 @@ struct RGB
 };
 
 // Operations pour les images noir et blanc
-int pgm_lire(char nom_fichier[], int matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_colonnes, int *p_maxval, struct MetaData *p_metadonnees);
-int pgm_ecrire(char nom_fichier[], int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes, int maxval, struct MetaData metadonnees);
-int pgm_copier(int matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, int matrice2[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes2, int *p_colonnes2);
-int pgm_creer_histogramme(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes, int histogramme[MAX_VALEUR+1]);
-int pgm_couleur_preponderante(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes);
-int pgm_eclaircir_noircir(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes, int maxval, int valeur);
-int pgm_creer_negatif(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes, int maxval);
-int pgm_extraire(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, int lignes2, int colonnes2, int *p_lignes, int *p_colonnes);
-int pgm_sont_identiques(int matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, int matrice2[MAX_HAUTEUR][MAX_LARGEUR], int lignes2, int colonnes2);
-int pgm_pivoter90(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_colonnes, int sens);
+int pgm_lire(char             nom_fichier[],
+             int              matrice[MAX_HAUTEUR][MAX_LARGEUR],
+             int             *p_lignes,
+             int             *p_colonnes,
+             int             *p_maxval,
+             struct MetaData *p_metadonnees);
+
+int pgm_ecrire(char            nom_fichier[],
+               int             matrice[MAX_HAUTEUR][MAX_LARGEUR],
+               int             lignes,
+               int             colonnes,
+               int             maxval,
+               struct MetaData metadonnees);
+
+int pgm_copier(int matrice1[MAX_HAUTEUR][MAX_LARGEUR],
+               int lignes1,
+               int colonnes1,
+               int matrice2[MAX_HAUTEUR][MAX_LARGEUR],
+               int *p_lignes2,
+               int *p_colonnes2);
+
+int pgm_creer_histogramme(int matrice[MAX_HAUTEUR][MAX_LARGEUR],
+                          int lignes,
+                          int colonnes,
+                          int histogramme[MAX_VALEUR+1]);
+
+int pgm_couleur_preponderante(int matrice[MAX_HAUTEUR][MAX_LARGEUR],
+                              int lignes,
+                              int colonnes);
+
+int pgm_eclaircir_noircir(int matrice[MAX_HAUTEUR][MAX_LARGEUR],
+                          int lignes,
+                          int colonnes,
+                          int maxval, int valeur);
+
+int pgm_creer_negatif(int matrice[MAX_HAUTEUR][MAX_LARGEUR],
+                      int lignes,
+                      int colonnes,
+                      int maxval);
+
+int pgm_extraire(int  matrice[MAX_HAUTEUR][MAX_LARGEUR],
+                 int  lignes1,
+                 int  colonnes1,
+                 int  lignes2,
+                 int  colonnes2,
+                 int *p_lignes,
+                 int *p_colonnes);
+
+int pgm_sont_identiques(int matrice1[MAX_HAUTEUR][MAX_LARGEUR],
+                        int lignes1,
+                        int colonnes1,
+                        int matrice2[MAX_HAUTEUR][MAX_LARGEUR],
+                        int lignes2,
+                        int colonnes2);
+
+int pgm_pivoter90(int  matrice[MAX_HAUTEUR][MAX_LARGEUR],
+                  int *p_lignes,
+                  int *p_colonnes,
+                  int  sens);
 
 // Operations pour les images couleurs
-int ppm_lire(char nom_fichier[], struct RGB matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_colonnes, int *p_maxval, struct MetaData *p_metadonnees);
-int ppm_ecrire(char nom_fichier[], struct RGB matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes, int maxval, struct MetaData metadonnees);
-int ppm_copier(struct RGB matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, struct RGB matrice2[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes2, int *p_colonnes2);
-int ppm_sont_identiques(struct RGB matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, struct RGB matrice2[MAX_HAUTEUR][MAX_LARGEUR], int lignes2, int colonnes2);
-int ppm_pivoter90(struct RGB matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_colonnes, int sens);
+int ppm_lire(char             nom_fichier[],
+             struct RGB       matrice[MAX_HAUTEUR][MAX_LARGEUR],
+             int             *p_lignes,
+             int             *p_colonnes,
+             int             *p_maxval,
+             struct MetaData *p_metadonnees);
+
+int ppm_ecrire(char            nom_fichier[],
+               struct RGB      matrice[MAX_HAUTEUR][MAX_LARGEUR],
+               int             lignes,
+               int             colonnes,
+               int             maxval,
+               struct MetaData metadonnees);
+
+int ppm_copier(struct RGB  matrice1[MAX_HAUTEUR][MAX_LARGEUR],
+               int         lignes1,
+               int         colonnes1,
+               struct RGB  matrice2[MAX_HAUTEUR][MAX_LARGEUR],
+               int        *p_lignes2,
+               int        *p_colonnes2);
+
+int ppm_sont_identiques(struct RGB matrice1[MAX_HAUTEUR][MAX_LARGEUR],
+                        int        lignes1,
+                        int        colonnes1,
+                        struct RGB matrice2[MAX_HAUTEUR][MAX_LARGEUR],
+                        int        lignes2,
+                        int        colonnes2);
+
+int ppm_pivoter90(struct RGB  matrice[MAX_HAUTEUR][MAX_LARGEUR],
+                  int        *p_lignes,
+                  int        *p_colonnes,
+                  int         sens);
 
 #endif
