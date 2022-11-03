841b0f421658d8ef373034f875a25b52e54bed9c
Benjamin Chausse <benjamin@chausse.xyz>
2022-11-03 13:31:58 -0400

    formatting

--- gestion_images.c
+++ gestion_images.c
@@ -17,40 +17,32 @@ struct RGB imageRGB2[MAX_HAUTEUR][MAX_LARGEUR];
 
 int main() {
 
-  test_pgm_lire();
+  AutoValidation();
 
-
-/*     int err = 0; */
-/*     int lignes1, colonnes1; */
-/*     int lignes2, colonnes2; */
-/*     int maxval; */
-/*     int histogramme[MAX_VALEUR+1]; */
-/*     char nom[MAX_CHAINE]; */
-/*     struct MetaData metadonnees; */
-
-
-/*     msg(INFO,"Debut!",0); */
-
-/*     // TODO: Remove this from production code */
-/*     strcpy(nom,"img/cat.pgm"); */
-
-/* 	// exemple d'appel de fonction */
-/*     err = pgm_lire(nom, image1, */
-/*                       &lignes1, &colonnes1, */
-/*                       &maxval, &metadonnees); */
-
-/* 	// exemple de traitement d'un code de retour (erreur ou reussite) */
-/* 	if (err == FALSE) */
-/* 		msg(INFO,"Aucune erreur.",OK); */
-/* 	else */
-/* 		msg(ERROR,"Un problème est survenu",ERREUR_FICHIER); */
-
-/* 	// autre exemple d'appel de fonction */
-/*     pgm_ecrire(nom, image1, */
-/*                lignes1, colonnes1, */
-/*                maxval, metadonnees); */
-
-/*     msg(INFO,"Fin!",OK); */
+    /* int err = 0; */
+    /* int lignes1, colonnes1; */
+    /* int lignes2, colonnes2; */
+    /* int maxval; */
+    /* int histogramme[MAX_VALEUR+1]; */
+    /* char nom[MAX_CHAINE]; */
+    /* struct MetaData metadonnees; */
+    /* msg(INFO,"Debut!",0); */
+    /* // TODO: Remove this from production code */
+    /* strcpy(nom,"img/cat.pgm"); */
+    /* // exemple d'appel de fonction */
+    /* err = pgm_lire(nom, image1, */
+    /*                   &lignes1, &colonnes1, */
+    /*                   &maxval, &metadonnees); */
+	/* // exemple de traitement d'un code de retour (erreur ou reussite) */
+	/* if (err == FALSE) */
+		/* msg(INFO,"Aucune erreur.",OK); */
+	/* else */
+		/* msg(ERROR,"Un problème est survenu",ERREUR_FICHIER); */
+	/* // autre exemple d'appel de fonction */
+    /* pgm_ecrire(nom, image1, */
+    /*            lignes1, colonnes1, */
+    /*            maxval, metadonnees); */
+    /* msg(INFO,"Fin!",OK); */
 
     return 0;
 }
